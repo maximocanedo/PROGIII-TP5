9dd6420c2d1f22d80795b56453de8c2d8e281958
Jean Esquen <jeanpierreesquen@gmail.com>
2023-05-01 02:58:32 -0300

    Clases

---
+++ Conexion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace TrabajoPractico5
+{
+    public class Conexion
+    {
+        string Ruta = "Data Source=PCOK\\SQLEXPRESS;Initial Catalog=BDSucursales;Integrated Security=True";
+
+        public SqlDataReader ObtenerDatos(String consulta)
+        {
+            SqlConnection conexion = new SqlConnection(Ruta);
+            SqlCommand command = new SqlCommand(consulta, conexion);
+            conexion.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            return reader;
+        }
+
+        public void cerrar(SqlDataReader cn)
+        {
+            cn.Close();
+        }
+        public int ejecutaTransaccion(String consulta)
+        {
+            SqlConnection conexion = new SqlConnection(Ruta);
+            conexion.Open();
+            SqlCommand comando = new SqlCommand(consulta, conexion);
+            int filasAfectadas = comando.ExecuteNonQuery();
+            return filasAfectadas;
+        }
+    }
+}
